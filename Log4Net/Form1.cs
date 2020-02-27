308fc3b6ec3c2c70a4acb1c72c17ec953d0a7185
raimonestanyol <61312986+raimonestanyol@users.noreply.github.com>
2020-02-27 19:00:14 +0100

    Log4Net, Forms and Filepath Example
    This exercise implements a windows forms app and a Log4Net logger. It also has a special app.config to store the FileName of a file stored in the compilation folder.

---
+++ Log4Net/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using log4net;
+using System.Configuration;
+
+namespace Log4Net
+{
+    public partial class Form1 : Form
+    {
+        private static readonly log4net.ILog logger = LogManager.GetLogger(typeof(Form1));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //This class is empty
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            logger.Info("whatever");
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            var sAttr = ConfigurationManager.AppSettings["FileName"].ToString();
+            MessageBox.Show(sAttr);
+        }
+    }
+}
